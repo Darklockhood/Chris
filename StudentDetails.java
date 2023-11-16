80ad4d4bc91c64fd9ad6e5e142710169b52bfca6
Darklockhood <150893986+Darklockhood@users.noreply.github.com>
2023-11-16 18:58:54 +0300

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,12 @@
+public class StudentDetails{
+public static void main(String[]args){
+System.out.println("Institution:KCA University");
+System.out.println("Course:DIT");
+System.out.println("Name:Ben Kidamani Omondi");
+System.out.println("Reg no:22/07534");
+System.out.println("Gender:Male");
+System.out.println("DOB:31.07.2002");
+System.out.println("Phone no:+254746997307");
+System.out.println("Email Address:omondiben648@gmail.com");
+}
+}
