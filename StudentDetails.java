f1e03e59005213163ac81413a9cf11926d36cc68
Darklockhood <150893986+Darklockhood@users.noreply.github.com>
2023-11-16 19:56:33 +0300

    Update StudentDetails.java

--- StudentDetails.java
+++ StudentDetails.java
@@ -2,11 +2,11 @@ public class StudentDetails{
 public static void main(String[]args){
 System.out.println("Institution:KCA University");
 System.out.println("Course:DIT");
-System.out.println("Name:Ben Kidamani Omondi");
-System.out.println("Reg no:22/07534");
+System.out.println("Name:christopher mbuvi joseph");
+System.out.println("Reg no:19/07026");
 System.out.println("Gender:Male");
-System.out.println("DOB:31.07.2002");
-System.out.println("Phone no:+254746997307");
-System.out.println("Email Address:omondiben648@gmail.com");
+System.out.println("DOB:23.03.2002");
+System.out.println("Phone no:0701647189");
+System.out.println("Email Address:ochristophermbuvi163@gmail.com");
 }
 }
